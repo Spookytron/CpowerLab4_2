9f247a71adc940488fa08b943100fa8c5e773146
Cameron Power <cameronpower@quickenloans.com>
2021-08-03 16:01:36 -0400

    lab 4_2 complete

---
+++ Lab4_2Rectangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//when running the tests I tried both the same numbers and different numbers in both rectangle variables and got the result "Different"
+// both times. When I set r1=r2, it returned the option same. I am assuming that this is because without setting r1 to r2 both rectangles
+// were hard coded and therefore took up different slots when created, and even though they contained the same basic parts and numbers,
+// they were inherently two different things. 
+
+
+
+namespace Lab4_2Rectangle
+{
+    class Rectangle
+    {
+        public int Length;
+        public int Width;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            Rectangle r2 = new Rectangle();
+            r1.Length = 10;
+            r2.Length = 10;
+            r1.Width = 10;
+            r2.Width = 10;
+            r1 = r2;
+            if (r1 == r2)
+            {
+                Console.WriteLine("Same");
+            }
+            else Console.WriteLine("Different");
+        }
+    }
+}
---
+++ Lab4_2_MenuItem/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Lab4_2_MenuItem
+{
+    class MenuItem
+    {
+        private int ID;
+        private string Name;
+        private string Description;
+        private decimal Price;
+
+
+
+        public MenuItem(int _ID, string _Name, string _Description, decimal _Price)
+        {
+            ID = _ID;
+            Name = _Name;
+            Description = _Description;
+            Price = _Price;
+        }
+        public void Print()
+        {
+            Console.WriteLine("\nMenuItem Info");
+            Console.WriteLine($"ID : {ID}");
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Price : {Price}");
+        }
+        public void setID(int _ID)
+        {
+            ID = _ID;
+        }
+        public void setName(string _Name)
+        {
+            if (_Name == " ")
+            {
+                Name = "EMPTY";
+            }
+            else
+            {
+                Name = _Name;
+            }
+        }
+        public void setDesc(string _Description)
+        {
+            if (_Description == " ")
+            {
+                Description = "EMPTY";
+            }
+            else
+            {
+                Description = _Description;
+            }
+        }
+        public void setPrice(decimal _Price)
+        {
+            if (_Price >= 0.50m && _Price <= 10.00m)
+            {
+                Price = _Price;
+            }
+            else if (_Price < 0.50m)
+            {
+                Price = 0.50m;
+            }
+            else
+            {
+                Price = 10.0m;
+            }
+        }
+        public decimal getPrice()
+        {
+            return Price;
+        }
+        public string getDescrip()
+        {
+            return Description;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public int getID()
+        {
+            return ID;
+        }
+        public void MenuConstruct(int _ID, string _Name, string _Description, decimal _Price)
+        {
+            ID = _ID;
+            Name = _Name;
+            Description = _Description;
+            Price = _Price;
+        }
+        public void EmptyConstruct(int _ID, string _Name, decimal _Price)
+        {
+            ID = _ID;
+            Name = _Name;
+            Description = "EMPTY";
+            Price = _Price;
+        }
+        public override string ToString()
+        {
+            return $"\nMenuItem Info\nID:{ID}\nName:{Name}\nDescription:{Description}\nPrice:{Price}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuItem Spicy = new MenuItem(17, "Potato", "One Potato", 0.49m);
+            Spicy.setPrice(20.0m);
+            Spicy.Print();
+            Spicy.MenuConstruct(30, "Wild", "Robot", 10.0m);
+            string potato = Spicy.ToString();
+            Console.WriteLine(potato);
+        }
+    }
+}
